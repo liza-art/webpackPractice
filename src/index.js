7ecff616c7b3bf57670de5c62348a3b12ffbc768
liza-art <lizagvozd70@gmail.com>
2026-03-11 07:08:27 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./styles.css";
+import { greeting } from "./greeting.js";
+// import odinImage from "./odin.png";
+
+// const image = document.createElement("img");
+// image.src = odinImage;
+
+console.log(greeting);
